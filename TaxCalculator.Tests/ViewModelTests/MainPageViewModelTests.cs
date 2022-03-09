d4a52339c09f620b11d0f8a1fb95474e0a55f8c1
Avery Wold <awold82@gmail.com>
2022-03-09 16:11:23 -0600

    Added test project

---
+++ TaxCalculator.Tests/ViewModelTests/MainPageViewModelTests.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Linq;
+using System.Threading.Tasks;
+using Moq;
+using NUnit.Framework;
+using TaxCalculator.Services;
+using TaxCalculator.ViewModels;
+using TaxCalculator.Views;
+using TaxCalculatorApp.Models;
+using Xamarin.Forms;
+
+namespace TaxCalculator.Tests.ViewModelTests
+{
+    [TestFixture]
+    public class MainPageViewModelTests
+    {
+        
+    }
+}
