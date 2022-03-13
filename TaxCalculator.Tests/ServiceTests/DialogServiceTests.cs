358d452df9aa66723613a82aa1d228a6457241df
Avery Wold <awold82@gmail.com>
2022-03-12 20:55:55 -0600

    added dialog service test

---
+++ TaxCalculator.Tests/ServiceTests/DialogServiceTests.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Threading.Tasks;
+using Moq;
+using NUnit.Framework;
+using TaxCalculator.Services;
+
+namespace TaxCalculator.Tests.ServiceTests
+{
+    [TestFixture]
+    public class DialogServiceTests
+    {
+        private Mock<DialogService> _dialogService;
+
+        [SetUp]
+        public void Setup()
+        {
+            _dialogService = new Mock<DialogService>();
+        }
+
+        [Test]
+        public void GetTaxRate_WithNoZip_DisplaysAlert()
+        {
+            // Arange
+            _dialogService.Setup(d => d.DisplayAlert(It.IsAny<string>())).Returns(Task.CompletedTask);
+
+            // Assert
+            _dialogService.Verify();
+        }
+    }
+}
